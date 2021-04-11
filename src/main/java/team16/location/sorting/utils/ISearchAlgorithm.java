d9c7edaddc19702ea576a3224dcf1916d0394fa1
greg1904 <67950803+greg1904@users.noreply.github.com>
2021-04-11 10:53:28 +0200

    Added Interface for Modules

--- src/main/java/team16/location/sorting/utils/ISearchAlgorithm.java
+++ src/main/java/team16/location/sorting/utils/ISearchAlgorithm.java
@@ -1,7 +1,5 @@
 package team16.location.sorting.utils;
 
-import team16.data.datainstances.packages.Package;
-
 public interface ISearchAlgorithm {
-    boolean checkPackage(Package packet);
+    boolean checkPackage(String stringOfContents);
 }
